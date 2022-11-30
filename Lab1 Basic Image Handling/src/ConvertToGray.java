ba22cfc2a4143d4387ee625ad585fd0201672234
Tirth <tirthacodes@gmail.com>
2022-11-30 22:23:42 +0545

    code written to convert image into gray

--- Lab1 Basic Image Handling/src/ConvertToGray.java
+++ Lab1 Basic Image Handling/src/ConvertToGray.java
@@ -1,9 +1,46 @@
+import java.io.File;
+import java.io.IOException;
+import java.awt.Color;
+import java.awt.image.BufferedImage;
+import javax.imageio.ImageIO;
 
 public class ConvertToGray {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello, World!");
+		//System.out.println("Hello, World!");
+		
+		BufferedImage image = null;
+		try {
+			image = ImageIO.read(new File("C:\\Tirtha Kshitz\\7th Sem\\IPPR\\7th Sem IPPR Labs\\TirthPic.jpg"));
+			System.out.println("Reading Complete.");
+		}
+		catch(IOException e) {
+			System.out.println("Error: " + e);
+		}
+		
+		for(int y=0;y<image.getHeight();y++)
+			for(int x=0;x<image.getWidth();x++)
+			{
+				Color c = new Color(image.getRGB(x, y));
+				int red = (c.getRed());
+				int green = (c.getGreen());
+				int blue = c.getBlue();
+				
+				int gray = (int)(red+green+blue)/3;
+				Color newColor = new Color(gray,gray,gray);
+				image.setRGB(x,y,newColor.getRGB());
+			}
+		
+		try {
+			File output_file = new File("C:\\Tirtha Kshitz\\7th Sem\\IPPR\\7th Sem IPPR Labs\\TirthPic_Greyed.jpg");
+			ImageIO.write(image, "jpg", output_file );
+			System.out.println("Writing Complete.");
+		}
+		catch(IOException e) {
+			System.out.println("Error: " + e);
+			return;
+		}
 	}
 
 }
